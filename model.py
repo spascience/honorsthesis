702678f285852fd91e8ff7f40f37fc65f3b9c13d
seanpaul <seanpaul@umich.edu>
2019-10-06 20:57:23 -0400

    Added model.py

---
+++ model.py
@@ -0,0 +1,40 @@
+"""
+model.py
+
+Model of harmonic composition in Western Tonal Music, based
+on formalization of Minimalist grammar by Chris Collins and Ed Stabler:
+
+Collins, C., & Stabler, E. (2016). A formalization of minimalist syntax.
+    Syntax, 19(1), 43-78.
+
+This model generates chord sequences via the assumption that the computational
+system for Human Language and the computational system for Human Music are
+actually the same. For more information, see:
+
+Katz, J., & Pesetsky, D. (2011). The identity thesis for language and music.
+    Manuscript, January.
+
+Mukherji, S. (2014). Generative Musical Grammar-A Minimalist Approach.
+    PhD Diss. Princeton University.
+
+
+
+Sean P. Anderson, seanpaul@umich.edu
+Generative Linguistics And Music grant
+October 6, 2019
+
+Done in work for undergraduate Honors Thesis
+"""
+
+class Lexicon:
+    """
+    yes, we are assuming Western Tonal Music has a lexicon
+    """
+    pass
+
+class Composer:
+    """
+    Composer class using Merge to generate harmonic sequences
+    """
+
+    pass
