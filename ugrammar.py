cb8706714ee6baa4ac1e8ce164e1bba6f5cd26c9
seanpaul <seanpaul@umich.edu>
2019-10-09 14:19:51 -0400

    Added ugrammar.py

---
+++ ugrammar.py
@@ -0,0 +1,14 @@
+"""
+ugrammar.py
+
+An implementation of Chris Collins and Ed Stabler's formalization of
+Minimalist Syntax.
+
+©2019 Sean Anderson
+seanpaul@umich.edu
+
+Generative Linguistics And Music grant
+Professors S. Mukherji, S. Epstein, and J. Zhang
+University of Michigan
+"""
+
