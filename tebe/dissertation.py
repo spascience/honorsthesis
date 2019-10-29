1076bfc178748958af3c92087ea3b854a13370c9
seanpaul <seanpaul@umich.edu>
2019-10-29 15:55:40 -0400

    Tebe: started on SyntObj, Merge, Filter

--- tebe/dissertation.py
+++ tebe/dissertation.py
@@ -9,7 +9,8 @@ in pages 337-365 of Prof. Mukherji's dissertation (2014).
 class Stufe:
     # Circle of Fifths in sharps, octave equivalence
     FIFTHS_NAMES = ['C','G','D','A','E','B',
-                    'F#','C#','G#','D#','A#','E#']
+                    'F#','C#','G#','D#','A#','F']
+    # TODO: Circle of Thirds relationships
 
     def __init__(self, cf=0, ct=0):
         # default ctor
@@ -26,6 +27,25 @@ class Stufe:
 
     def __str__(self):
         return f"{self.name}: c5 = {self.cf}, c3 = {self.ct}"
+
+class SyntacticObject:
+    def __init__(self, m1, m2):
+        """
+        represents the output of Merge.
+
+        :param m1: a Stufe or SyntacticObject
+        :param m2: a Stufe or SyntacticObject
+        """
+
+        self.items = (m1, m2)
+
+        # latter object projects syntactic features
+        self.cf = m2.cf
+        self.ct = m2.ct
+
+    def __str__(self):
+        # recursively access contained stufen
+        return f"[{str(self.items[0])}, {str(self.items[1])}]"
 
 
 class Model:
@@ -49,7 +69,43 @@ class Model:
         """
 
         if self.merge_positive:
-            # TODO: implment
-            return False
+            # western tonal
+            return (s2.cf + 1) == s1.cf
+            # TODO: thirds relationship
+        else:
+            # rock
+            return (s1.cf + 1) == s2.cf
+            # TODO: thirds relationship
 
-        raise NotImplementedError
+    def merge(self, s1, s2):
+        """
+        R: agree(s1, s2)
+
+        :param s1: Stufe or SyntacticObject
+        :param s2: Stufe or SyntacticObject
+        :return: SyntacticObject
+        """
+        return SyntacticObject(s1, s2)
+
+    def filter(self, workspace):
+        """
+        later: cycle-based derivation?
+        Checks if current derivation contains the Ursatz
+        as its deepest structure.
+        (yes, this is a scaringly large filter)
+
+
+        :param workspace: SyntacticObject
+        :return: boolean
+        """
+
+        # should work for any key
+        tonic_cf = workspace.cf
+        tonic_ct = workspace.ct
+
+        if self.merge_positive:
+            if workspace.items[0].cf == tonic_cf \ # tonic prolongation
+                and workspace.items[1].items[0] == tonic_cf + 1:
+                # Tonic Prolongation, Dominant Prolongation, Tonic Completion
+                return True
+        else: return False
