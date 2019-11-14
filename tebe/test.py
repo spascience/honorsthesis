54a2734ace218646df17f0805e2d643e6f71fb92
seanpaul <seanpaul@umich.edu>
2019-11-13 21:58:13 -0500

    Added test.py in tebe

---
+++ tebe/test.py
@@ -0,0 +1,22 @@
+"""
+test.py
+
+A simple test of the Tebe generative model.
+
+11/13/2019 Sean Anderson
+"""
+
+from tebe import dissertation
+
+def main():
+
+    composer = dissertation.Model()
+
+    compositions = composer.generate_v3(n=1)
+
+    composer.spell_out(compositions)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
