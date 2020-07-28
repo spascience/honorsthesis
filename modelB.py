f6b253f437b8dd92f37fb7e9c2479fbf42cdc10c
Sean Anderson <seanpaul@umich.edu>
2020-07-27 23:18:48 -0400

    Added, drafted modelB (mukherji, 2014)

---
+++ modelB.py
@@ -0,0 +1,127 @@
+"""
+modelB.py
+
+Model of harmonic composition in Western Tonal Music, based
+on formalization of Minimalist grammar by Chris Collins and Ed Stabler:
+
+Collins, C., & Stabler, E. (2016). A formalization of minimalist syntax.
+    Syntax, 19(1), 43-78.
+
+This model generates chord sequences via the assumption that the computational
+system for Human Language and the computational system for Human Music are
+actually the same. For more information, see:
+
+Katz, J., & Pesetsky, D. (2011). The identity thesis for language and music.
+    Manuscript, January.
+
+Mukherji, S. (2014). Generative Musical Grammar-A Minimalist Approach.
+    PhD Diss. Princeton University.
+
+modelB derives with feature-driven Merge (Agree), closer to the
+Mukherji (2014) model.
+
+Sean P. Anderson, seanpaul@umich.edu
+Generative Linguistics And Music grant
+Professors S. Mukherji, S. Epstein, and J. Zhang, University of Michigan
+July 26, 2020
+
+Done in work for undergraduate Honors Thesis
+"""
+
+import random
+import itertools
+
+from model import Composer, Stufe, SyntacticObject
+
+
+class ComposerB(Composer):
+    # Mukherji (2014) model
+    def agree(self, so1, so2) -> bool:
+        """
+        Stufen-based Agree. Returns whether so1 and
+        so2 are Merge-able in the order specified.
+
+        :param so1: Stufe or SyntacticObject
+        :param so2: Stufe or SyntacticObject
+        :return: bool
+        """
+
+        # Western Tonal music parameterization
+        do_agree = (0 <= so2.c5 - so1.c5 <= 1) or (0 <= so1.c3 - so1.c5 <= 1)
+        return do_agree
+
+    def get_mergables(self, stage: Composer.Stage) -> (bool, list):
+        """
+        Checks if a merge is possible with items in stage.workspace.
+        Returns false if no possible merges exist. Returns a list of
+        SO pairs if true.
+
+        :param stage: Composer.Stage
+        :return: bool, list of tuples
+        """
+
+        success = False
+        merges_possible = list()
+        # order matters
+        for so1, so2 in itertools.permutations(stage.workspace, r=2):
+            if self.agree(so1, so2):
+                success = True
+                merges_possible.append(tuple(so1, so2))
+
+        return success, merges_possible
+
+    def derive(self, la, verbose=True):
+        """
+        Executes a derivation starting with Lexical Array la.
+        Flips a coin to decide whether to Select or to Merge.
+        If Merging, merges agreeing SO's if possible, otherwise
+        makes no operation. Every SO generated that passes Filter
+        will be spelled out.
+
+        :param la: collection of Stufe objs
+        :param verbose: bool
+        :return: collection of derivations, bool
+        """
+
+        if len(la) < 2:
+            print("Error: You need more than 2 Stufen to compose")
+            return list()
+
+        # set up (select 2)
+        derivations = list()
+        current = Composer.Stage(la=set(la), workspace=set())
+        current = self.select_random(current)
+        current = self.select_random(current)
+        self.stage_i = 2
+
+        # derivation
+        while len(current.la) > 0 or len(current.workspace) != 1:
+            flip = random.choice([0,1])
+            if flip and len(current.la) > 0:
+                # Select
+                current = self.select_random(current)
+            elif not flip and len(current.workspace) != 1:
+                # Merge
+                merge_possible, mergeables = self.get_mergables(current)
+                if merge_possible:
+                    so1, so2 = random.choice(mergeables)
+                    new_so = self.merge(so1, so2, current)
+                    # Filter and spell out
+                    if self.filter(new_so):
+                        # found a valid derivation!
+                        derivations.append(new_so)
+
+            self.stage_i += 1
+            if verbose:
+                print(f"Stage #{self.stage_i}:")
+                print(current)
+                print()
+
+        # end of derivation
+        if self.filter(list(current.workspace)[0]):  # awk
+            print("Derivation finished")
+            return derivations, True
+        else:
+            # derivation crashed
+            print("Derivation crashed")
+            return derivations, False
